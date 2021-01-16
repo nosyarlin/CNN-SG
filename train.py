26bea2f20b0613bce131aa60afad796612ecaf8a
Rayson Lim Jun Kai <rayson@jublia.com>
2021-01-16 13:57:24 +0800

    Fix formatting lints

--- train.py
+++ train.py
@@ -25,10 +25,10 @@ def evaluate_model(model, dl, loss_func, device, logger_title):
 
         probabilities = []
 
-        j = 0 #Starting the iteration count
+        j = 0  # Starting the iteration count
 
         for X, y in dl:
-            j += 1 #Adding one iteration count per batch
+            j += 1  # Adding one iteration count per batch
 
             X, y = X.to(device), y.to(device)
 
@@ -44,9 +44,9 @@ def evaluate_model(model, dl, loss_func, device, logger_title):
             probabilities.append(softmax(logits))
 
             Logger.current_logger().report_scalar(
-                logger_title, "loss", iteration= j, value= loss.item())
+                logger_title, "loss", iteration=j, value=loss.item())
             Logger.current_logger().report_scalar(
-                logger_title, "accuracy", iteration= j, value= (total_correct / total_count))
+                logger_title, "accuracy", iteration=j, value=(total_correct / total_count))
 
     probabilities = torch.cat(probabilities, 0)
 
@@ -59,10 +59,10 @@ def train_model(model, dl, loss_func, optimizer, device, archi, epoch):
     total_count = 0
     total_correct = 0
 
-    j = 0 #Starting the iteration count 
+    j = 0  # Starting the iteration count
 
     for X, y in dl:
-        j += 1 #Adding one iteration count per batch
+        j += 1  # Adding one iteration count per batch
 
         model.zero_grad()
         X, y = X.to(device), y.to(device)
@@ -86,9 +86,9 @@ def train_model(model, dl, loss_func, optimizer, device, archi, epoch):
         optimizer.step()
 
         Logger.current_logger().report_scalar(
-            "Training", "loss", iteration=((epoch+1) * j), value= loss.item())
+            "Training", "loss", iteration=((epoch + 1) * j), value=loss.item())
         Logger.current_logger().report_scalar(
-            "Training", "accuracy", iteration=((epoch+1) * j), value= (total_correct / total_count))
+            "Training", "accuracy", iteration=((epoch + 1) * j), value=(total_correct / total_count))
 
     return total_correct / total_count, np.mean(train_loss)
 
@@ -111,31 +111,33 @@ def train_validate(
         acc_train, loss_train = train_model(
             model, train_dl, loss_func, optimizer, device, archi, epoch)
         train_acc.append(acc_train)
-        train_loss.append(loss_train)       
+        train_loss.append(loss_train)
         scheduler.step()
 
         # Validate
-        acc_val, loss_val, probabilities = evaluate_model(model, val_dl, loss_func, device, 'Validation (Most recent epoch)')
+        acc_val, loss_val, probabilities = evaluate_model(
+            model, val_dl, loss_func, device, 'Validation (Most recent epoch)')
         val_acc.append(acc_val)
         val_loss.append(loss_val)
-        
+
         # Save model if improved
         if not best_weights or val_acc[-1] > best_val_acc:
             best_weights = model.state_dict()
-            torch.save(best_weights, os.path.join(path_to_save_results, 'model.pth'))
+            torch.save(best_weights, os.path.join(
+                path_to_save_results, 'model.pth'))
             best_val_acc = val_acc[-1]
         else:
             print("Model has not improved, and will not be saved.\n")
 
         # Logging the results in clearml
         Logger.current_logger().report_scalar(
-            "Training and Validation", "Train accuracy", iteration= epoch+1, value= acc_train)
+            "Training and Validation", "Train accuracy", iteration=epoch + 1, value=acc_train)
         Logger.current_logger().report_scalar(
-            "Training and Validation", "Train loss", iteration= epoch+1, value= loss_train)
+            "Training and Validation", "Train loss", iteration=epoch + 1, value=loss_train)
         Logger.current_logger().report_scalar(
-            "Training and Validation", "Val accuracy", iteration= epoch+1, value= acc_val)
+            "Training and Validation", "Val accuracy", iteration=epoch + 1, value=acc_val)
         Logger.current_logger().report_scalar(
-            "Training and Validation", "Val loss", iteration= epoch+1, value= loss_val)
+            "Training and Validation", "Val loss", iteration=epoch + 1, value=loss_val)
 
         print("Epoch: {} of {}".format(epoch + 1, epochs))
         print("Validation acc: {}, Validation loss: {}"
@@ -149,32 +151,53 @@ def train_validate(
 
 
 if __name__ == '__main__':
-    # Connecting to the clearml dashboard 
-    task = Task.init(project_name="Nosyarlin", task_name= "Train_"+ date.today().strftime('%Y-%m-%d'),
-        task_type= Task.TaskTypes.training)
+    # Connecting to the clearml dashboard
+    task = Task.init(project_name="Nosyarlin", task_name="Train_" + date.today().strftime('%Y-%m-%d'),
+                     task_type=Task.TaskTypes.training)
 
     # Set hyperparameters
-    parser = argparse.ArgumentParser(description='Process Command-line Arguments')
-    parser.add_argument('--image_dir', default= None, action= 'store', help= 'Path to the directory containing the images')
-    parser.add_argument('--path_to_save_results', default= None, action= 'store', help= 'Path to the directory to save the model, hyperparameters and results')
-    parser.add_argument('--run_test', default= True, type= bool, action= 'store', help= 'Determine if testing should be conducted')
-    parser.add_argument('--archi', default= 'resnet50', action='store', help='Architecture of the model to be trained. Either inception, resnet50, wide_resnet50, or mobilenet')
-    parser.add_argument('--pretrained', default= True, type= bool, action='store', help='Choose if the model to be trained should be a pretrained model from pytorch')
-    parser.add_argument('--train_all_weights', default= True, type= bool, action='store', help='True: train all weights across all layers; False: train classification layer only')
-    parser.add_argument('--use_data_augmentation', default= True, type= bool, action='store', help='Using data augmentation')
-    parser.add_argument('--use_gpu', default= True, type= bool, action='store', help='Using GPU for processing')
-    parser.add_argument('--num_classes', default= '3', type= int, action='store', help='Number of classes to be trained')
-    parser.add_argument('--lr', default= '0.001', type= float, action='store', help='The learning rate')
-    parser.add_argument('--betadist_alpha', default= 0.9, type= float, action='store', help='The alpha value controlling the shape of the beta distribution for the Adam optimiser')
-    parser.add_argument('--betadist_beta', default= 0.99, type= float, action='store', help='The beta value controlling the shape of the beta distribution for the Adam optimiser')
-    parser.add_argument('--eps', default= '1e-8', type= float, action='store', help='Epsilon value for Adam optimiser')
-    parser.add_argument('--weight_decay', default= '0', type= float, action='store', help='Weight decay for Adam optimiser')
-    parser.add_argument('--epochs', default= '25', type= int, action='store', help='Number of epochs to be run for training')
-    parser.add_argument('--step_size', default= '5', type= int, action='store', help='Step size')
-    parser.add_argument('--gamma', default= '0.1', type= float, action='store', help='Gamma value for optimiser')
-    parser.add_argument('--batch_size', default= '32', type= int, action='store', help='Batch size for training')
-    parser.add_argument('--img_size', default= '360', type= int, action='store', help='Image size for each image')
-    parser.add_argument('--crop_size', default= '299', type= int, action='store', help='Crop size for each image. Inception v3 expects 299')
+    parser = argparse.ArgumentParser(
+        description='Process Command-line Arguments')
+    parser.add_argument('--image_dir', default=None, action='store',
+                        help='Path to the directory containing the images')
+    parser.add_argument('--path_to_save_results', default=None, action='store',
+                        help='Path to the directory to save the model, hyperparameters and results')
+    parser.add_argument('--run_test', default=True, type=bool,
+                        action='store', help='Determine if testing should be conducted')
+    parser.add_argument('--archi', default='resnet50', action='store',
+                        help='Architecture of the model to be trained. Either inception, resnet50, wide_resnet50, or mobilenet')
+    parser.add_argument('--pretrained', default=True, type=bool, action='store',
+                        help='Choose if the model to be trained should be a pretrained model from pytorch')
+    parser.add_argument('--train_all_weights', default=True, type=bool, action='store',
+                        help='True: train all weights across all layers; False: train classification layer only')
+    parser.add_argument('--use_data_augmentation', default=True,
+                        type=bool, action='store', help='Using data augmentation')
+    parser.add_argument('--use_gpu', default=True, type=bool,
+                        action='store', help='Using GPU for processing')
+    parser.add_argument('--num_classes', default='3', type=int,
+                        action='store', help='Number of classes to be trained')
+    parser.add_argument('--lr', default='0.001', type=float,
+                        action='store', help='The learning rate')
+    parser.add_argument('--betadist_alpha', default=0.9, type=float, action='store',
+                        help='The alpha value controlling the shape of the beta distribution for the Adam optimiser')
+    parser.add_argument('--betadist_beta', default=0.99, type=float, action='store',
+                        help='The beta value controlling the shape of the beta distribution for the Adam optimiser')
+    parser.add_argument('--eps', default='1e-8', type=float,
+                        action='store', help='Epsilon value for Adam optimiser')
+    parser.add_argument('--weight_decay', default='0', type=float,
+                        action='store', help='Weight decay for Adam optimiser')
+    parser.add_argument('--epochs', default='25', type=int,
+                        action='store', help='Number of epochs to be run for training')
+    parser.add_argument('--step_size', default='5', type=int,
+                        action='store', help='Step size')
+    parser.add_argument('--gamma', default='0.1', type=float,
+                        action='store', help='Gamma value for optimiser')
+    parser.add_argument('--batch_size', default='32', type=int,
+                        action='store', help='Batch size for training')
+    parser.add_argument('--img_size', default='360', type=int,
+                        action='store', help='Image size for each image')
+    parser.add_argument('--crop_size', default='299', type=int, action='store',
+                        help='Crop size for each image. Inception v3 expects 299')
 
     args = parser.parse_args([
         '--image_dir', 'C:/_for-temp-data-that-need-SSD-speed/ProjectMast_FYP_Media',
@@ -184,12 +207,12 @@ if __name__ == '__main__':
         '--epochs', '15',
         '--lr', '0.001',
         '--betadist_alpha', '0.9',
-        '--betadist_beta', '0.99', 
-        '--batch_size', '32', 
+        '--betadist_beta', '0.99',
+        '--batch_size', '32',
         '--weight_decay', '0'
-        ])
+    ])
 
-    # Check that paths to save results and models exist 
+    # Check that paths to save results and models exist
     if path.exists(args.path_to_save_results):
         print("\nSaving results in " + args.path_to_save_results)
     else:
@@ -212,28 +235,31 @@ if __name__ == '__main__':
         X_test, y_test, args.batch_size, args.image_dir, args.img_size, args.crop_size, False
     )
 
-    print("Dataset to be used includes {} training images, {} validation images and {} testing images.".format(len(X_train), len(X_val), len(X_test)))
+    print("Dataset to be used includes {} training images, {} validation images and {} testing images.".format(
+        len(X_train), len(X_val), len(X_test)))
     print("Number of empty:humans:animals in training, validation and testing sets respectively is: {}:{}:{}; {}:{}:{}; {}:{}:{}\n".format(
-        y_train.count("0"),y_train.count("1"),y_train.count("2"),
-        y_val.count("0"),y_val.count("1"),y_val.count("2"),
-        y_test.count("0"),y_test.count("1"),y_test.count("2")))
+        y_train.count("0"), y_train.count("1"), y_train.count("2"),
+        y_val.count("0"), y_val.count("1"), y_val.count("2"),
+        y_test.count("0"), y_test.count("1"), y_test.count("2")))
     if args.run_test:
         print('Testing will be conducted.')
     else:
         print('Testing will NOT be conducted')
-        
+
     # Output hyperparameters for recording purposes
     hp_names = (
         "Run_test", "LearningRate", "BetaDist_alpha", "BetaDist_beta", "Eps", "WeightDecay", "Epochs", "StepSize", "Gamma", "BatchSize", "ImgSize", "CropSize",
         "Architecture", "NumClasses", "UseDataAugmentation", "TrainAllWeights", "Pretrained",
         "NumTrainImages", "NumValImages", "NumTestImages")
     hp_values = (
-        args.run_test, args.lr, args.betadist_alpha, args.betadist_beta, args.eps, args.weight_decay, args.epochs, args.step_size, args.gamma, args.batch_size, args.img_size, args.crop_size, 
+        args.run_test, args.lr, args.betadist_alpha, args.betadist_beta, args.eps, args.weight_decay, args.epochs, args.step_size, args.gamma, args.batch_size, args.img_size, args.crop_size,
         args.archi, args.num_classes, args.use_data_augmentation, args.train_all_weights, args.pretrained,
         len(X_train), len(X_val), len(X_test))
 
-    hp_records = pd.DataFrame({'Hyperparameters': hp_names, 'Values': hp_values})
-    hp_records.to_csv(index=False, path_or_buf= os.path.join(args.path_to_save_results, 'hyperparameter_records.csv'))
+    hp_records = pd.DataFrame(
+        {'Hyperparameters': hp_names, 'Values': hp_values})
+    hp_records.to_csv(index=False, path_or_buf=os.path.join(
+        args.path_to_save_results, 'hyperparameter_records.csv'))
 
     # Build model
     model, parameters = get_model(
@@ -264,9 +290,9 @@ if __name__ == '__main__':
     )
 
     # Train and validate
-    weights, train_loss, train_acc, val_loss, val_acc, train_val_results= train_validate(
+    weights, train_loss, train_acc, val_loss, val_acc, train_val_results = train_validate(
         args.epochs, model, optimizer, scheduler, loss_func,
-        train_dl, val_dl, device, args.archi, 
+        train_dl, val_dl, device, args.archi,
         args.path_to_save_results
     )
 
@@ -275,24 +301,27 @@ if __name__ == '__main__':
     write_to_csv(val_loss, 'val_loss.csv')
     write_to_csv(val_acc, 'val_acc.csv')
     train_val_results.to_csv(
-        index=False, path_or_buf = os.path.join(args.path_to_save_results, 'train_val_results.csv'))
+        index=False, path_or_buf=os.path.join(args.path_to_save_results, 'train_val_results.csv'))
 
     # Test
     if args.run_test:
         print("Training and validation complete. Starting testing now.")
-        
+
         test_acc_data = []
-        test_loss_data = []        
+        test_loss_data = []
         model.load_state_dict(weights)
-        test_acc, test_loss, probabilities = evaluate_model(model, test_dl, loss_func, device, 'Testing')
+        test_acc, test_loss, probabilities = evaluate_model(
+            model, test_dl, loss_func, device, 'Testing')
         print("Test acc: {}, Test loss: {}".format(test_acc, test_loss))
         test_acc_data.append(test_acc)
         test_loss_data.append(test_loss)
 
         # Saving results and probabilities into datasets
-        test_results = pd.DataFrame({'Acc': test_acc_data, 'Loss': test_loss_data})
-        test_results.to_csv(index=False, path_or_buf = os.path.join(args.path_to_save_results, 'test_results.csv'))
-        
+        test_results = pd.DataFrame(
+            {'Acc': test_acc_data, 'Loss': test_loss_data})
+        test_results.to_csv(index=False, path_or_buf=os.path.join(
+            args.path_to_save_results, 'test_results.csv'))
+
         with open(os.path.join(args.path_to_save_results, 'test_probabilities.csv'), 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(["prob_empty", "prob_human", "prob_animal"])
