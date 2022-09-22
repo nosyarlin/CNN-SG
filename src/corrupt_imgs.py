4061757137081e78a2389d0fcabad3faa79be425
JoejynWan <joejyn@gmail.com>
2022-09-22 17:06:14 +0800

    Add instance where FileName is a full path

--- src/corrupt_imgs.py
+++ src/corrupt_imgs.py
@@ -5,11 +5,16 @@ from tqdm import tqdm
 from config import LABELS_FILEPATH, IMAGE_DIR
 
 
+print("Checking for corrupt images now...")
 img_datasheet = pd.read_csv(LABELS_FILEPATH)
 
 for filename in tqdm(img_datasheet.FileName):
     try:
-        path = os.path.join(IMAGE_DIR, filename)
+        if os.path.exists(filename): #filename is a full path
+            path = filename
+        else: #images are saved in IMAGE_DIR and filename not a full path
+            path = os.path.join(IMAGE_DIR, filename)
+
         img = Image.open(path)
         img.verify()
     except (IOError, SyntaxError):
