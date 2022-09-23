99f400eaaaa8bc2baff38e598c49cc563db63531
JoejynWan <joejyn@gmail.com>
2022-09-23 23:10:58 +0800

    Add multiprocessing and adding image_size argument

--- src/config.py
+++ src/config.py
@@ -5,11 +5,15 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent.parent
 IMAGE_DIR = os.path.join(ROOT_DIR, 'data', 'images')
 MODEL_DIR = os.path.join(ROOT_DIR, 'models')
-PREPROCESSED_IMAGE_DIR = os.path.join(ROOT_DIR, 'data', 'preprocessed_images')
+# PREPROCESSED_IMAGE_DIR = os.path.join(ROOT_DIR, 'data', 'preprocessed_images')
+PREPROCESSED_IMAGE_DIR = 'C:\\temp_for_speed\\CNN-SG\\Big3_train'
 RESULTS_DIR = os.path.join(ROOT_DIR, 'results')
 SPLITS_DIR = os.path.join(ROOT_DIR, 'data', 'splits')
 
 # File paths
-LABELS_FILEPATH = os.path.join(ROOT_DIR, 'data', 'labels.csv')
+LABELS_FILEPATH = os.path.join(ROOT_DIR, 'data', '20220922_big3_train.csv')
 MODEL_FILEPATH = os.path.join(MODEL_DIR, 'model.pth')
 HYPERPARAMETERS_FILEPATH = os.path.join(RESULTS_DIR, 'hyperparameters.csv')
+
+# Settings for resize.py
+IMAGE_SIZE = 360
--- src/resize.py
+++ src/resize.py
@@ -2,13 +2,26 @@ import os
 import pandas as pd
 from PIL import Image
 from tqdm import tqdm
-from config import LABELS_FILEPATH, PREPROCESSED_IMAGE_DIR, IMAGE_DIR
+import multiprocessing as mp
+from config import LABELS_FILEPATH, PREPROCESSED_IMAGE_DIR, IMAGE_DIR, IMAGE_SIZE
+
+
+def resize_img(i, input_sheet):
+    path = os.path.join(IMAGE_DIR, input_sheet.FileName[i])
+    image = Image.open(path)
+    w, h = image.size
+
+    if w < h:
+        new_h = int(IMAGE_SIZE * h / w)
+        image_resized = image.resize((IMAGE_SIZE, new_h))
+    else:
+        new_w = int(IMAGE_SIZE * w / h)
+        image_resized = image.resize((new_w, IMAGE_SIZE))
+
+    image_resized.save(input_sheet.FileName_resized[i])
 
 
 if __name__ == '__main__':
-    # Setting Inputs
-    img_size = 360
-
     # Creating the datasheet
     input_sheet = pd.read_csv(LABELS_FILEPATH)
 
@@ -23,21 +36,22 @@ if __name__ == '__main__':
 
     # Resizing and saving the images
     print("Resizing images by making the shorter of width or height {} pixels. " \
-        "Aspect ratio of each image is thus maintained.".format(img_size))
-    for i in tqdm(range(len(input_sheet))):
-        path = os.path.join(IMAGE_DIR, input_sheet.FileName[i])
-        image = Image.open(path)
-        w, h = image.size
+        "Aspect ratio of each image is thus maintained.".format(IMAGE_SIZE))
+    
+    results = []
+    def callback_func(result):
+        results.append(result)
+        pbar.update()
 
-        if w < h:
-            new_h = int(img_size * h / w)
-            image_resized = image.resize((img_size, new_h))
-        else:
-            new_w = int(img_size * w / h)
-            image_resized = image.resize((new_w, img_size))
+    pool = mp.Pool(processes=mp.cpu_count())
+    pbar = tqdm(total = len(input_sheet))
 
-        image_resized.save(input_sheet.FileName_resized[i])
+    for i in range(len(input_sheet)):
+        pool.apply_async(resize_img, args=(i, input_sheet), callback=callback_func)
 
+    pool.close()
+    pool.join()
+    
     # Saving out the datasheet
     input_sheet['FileName'] = input_sheet['FileName_resized']
     input_sheet = input_sheet.drop(columns='FileName_resized')
