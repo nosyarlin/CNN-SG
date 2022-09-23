b93420bdddb7550eb0e3d4ffd8571c914410e71b
JoejynWan <joejyn@gmail.com>
2022-09-23 11:34:12 +0800

    Remove redundant code

--- src/corrupt_imgs.py
+++ src/corrupt_imgs.py
@@ -10,11 +10,7 @@ img_datasheet = pd.read_csv(LABELS_FILEPATH)
 
 for filename in tqdm(img_datasheet.FileName):
     try:
-        if os.path.exists(filename): #filename is a full path
-            path = filename
-        else: #images are saved in IMAGE_DIR and filename not a full path
-            path = os.path.join(IMAGE_DIR, filename)
-
+        path = os.path.join(IMAGE_DIR, filename)
         img = Image.open(path)
         img.verify()
     except (IOError, SyntaxError):
