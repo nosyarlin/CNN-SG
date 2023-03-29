21b1573af799864d37affb48677318ca62bac92b
Joejyn Wan <joejyn@gmail.com>
2023-03-29 18:02:28 +0800

    Get main function from compile_model.py

--- src/compile_model.py
+++ src/compile_model.py
@@ -7,6 +7,33 @@ from shared_funcs import load_checkpoint
 from config import MODEL_FILEPATH, HYPERPARAMETERS_FILEPATH, RESULTS_DIR
 
 
+def pth_to_pt():
+    # Get hyperparameters of the saved model
+    hp = pd.read_csv(args.hyperparameters)
+    archi = hp.loc[hp['Hyperparameters']
+                    == 'Architecture', 'Values'].item()
+    num_classes = int(hp.loc[hp['Hyperparameters']
+                        == 'NumClasses', 'Values'].item())
+    dropout = float(hp.loc[hp['Hyperparameters']
+                    == 'Dropout', 'Values'].item())
+
+    # Load and compile model
+    model, _ = get_model(archi, num_classes, False, False, dropout)
+    load_checkpoint(args.model_path, model)
+
+    model.eval()
+    example = torch.rand(1, 3, 320, 480)
+    traced_script_module = torch.jit.trace(model, example)
+
+    # Save compiled model
+    model_name = os.path.basename(args.model_path)
+    model_name = os.path.splitext(model_name)[0]
+    model_save_path = os.path.join(args.results_path, model_name + ".pt")
+    traced_script_module.save(model_save_path)
+
+    print("Model compiled and saved at", model_save_path)
+
+
 def get_arg_parser():
     parser = argparse.ArgumentParser(
         description='Process Command-line Arguments')
@@ -34,21 +61,4 @@ if __name__ == '__main__':
     parser = get_arg_parser()
     args = parser.parse_args()
 
-    # Get hyperparameters of the saved model
-    hp = pd.read_csv(args.hyperparameters)
-    archi = hp.loc[hp['Hyperparameters']
-                    == 'Architecture', 'Values'].item()
-    num_classes = int(hp.loc[hp['Hyperparameters']
-                        == 'NumClasses', 'Values'].item())
-    dropout = float(hp.loc[hp['Hyperparameters']
-                    == 'Dropout', 'Values'].item())
-
-    model, _ = get_model(archi, num_classes, False, False, dropout)
-    load_checkpoint(args.model_path, model)
-
-    model.eval()
-    example = torch.rand(1, 3, 320, 480)
-    traced_script_module = torch.jit.trace(model, example)
-
-    model_save_path = os.path.join(args.results_path, "model.pt")
-    traced_script_module.save(model_save_path)
+    pth_to_pt()
